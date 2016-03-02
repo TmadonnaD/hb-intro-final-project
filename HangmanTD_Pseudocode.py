cf3745c78114f3c2cfb3689b7047d8b3f05490f8
TmadonnaD <terrydemidio@yahoo.com>
2016-03-01 18:33:08 -0800

    Added files via upload
    Pseudocode

---
+++ HangmanTD_Pseudocode.py
@@ -0,0 +1,56 @@
+# HangmanTD
+
+# Major Features/Requirements:
+
+# Design a Word-guessing game that:
+
+# 	~ Invites player to "sign-in"
+# 	~ Genrates a random word, 
+# 	~ Tracks user input
+# 	~ Allows X# of guesses, 
+# 	~ Determines if player has guessed correctly or not. 
+#  	~ Keeps track of players score.(wins/losses).
+
+# Expected Code Features:
+
+# 	~Modules, Lists, Loops, Conditionals, Simple Graphics, ?
+
+
+# PSEUDOCODE:
+
+# Obtain Player's name
+
+# Store player's name.
+ 
+# Randomly generate a word from a defined universe of words.
+
+# Tell/show player the length of the word (how many letters).
+
+# Ask player to guess a letter.
+
+# Compare platyers input/determine whether the letter is in the word.
+
+# If yes, fill in the letter space(s).
+# If no, 
+#		~ increment "word guess" +1 
+#		~ draw a body part 
+#		~ Ask for new letter.
+#	Continue until word is guessed OR guesses exceed X.
+
+#   If guesses exceed X before word is guessed, "Sorry, try again?"
+#		increment- add 1 to "Losses/Misses" counter 
+#	If word is guessed correctly,  "You got it!" 
+	#	increment add 1 to "Wins/Hits" counter.
+
+
+
+# Special situations:
+# 	Players with same name
+
+# Note /Questions: 
+# Scope
+#	Display letters that have been guessed?
+#	Different word lists for Kids/Adults?
+
+
+
